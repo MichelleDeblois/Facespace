07a7a1532c9caa29b64d31d568ac0c5b202f5652
MichelleDeblois <michelledeblois93@gmail.com>
2022-03-06 14:28:45 -0500

    error message

--- frontend/src/components/ErrorMessage.js
+++ frontend/src/components/ErrorMessage.js
@@ -1,6 +1,10 @@
 import React from "react";
-
+import styled from "styled-components";
 const ErrorMessage = () => {
-  return <div> error</div>;
+  return <Message> Oops this user does not have an account</Message>;
 };
+
+const Message = styled.div`
+  color: red;
+`;
 export default ErrorMessage;
